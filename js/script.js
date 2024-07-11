6f992f5da5e17093eb858df5e3c366dfba8640c7
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-07-10 21:21:06 -0700

    logic to update amount for dishes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 const dataResponse = await fetch("http://127.0.0.1:5500/data.json"); // for quokka testing
 // const dataResponse = await fetch("../data.json");  for final live website
 const data = await dataResponse.json();
-console.log(data);
+const plusBtns = document.querySelectorAll(".plus");
+const minusBtns = document.querySelectorAll(".minus");
+
+// console.log(data);
+
+const updateAmount = (e) => {
+  const amount = e.currentTarget.parentElement.querySelector(".amount");
+  let newVal = parseInt(e.currentTarget.getAttribute("data-amount"));
+  let updatedAmount = parseInt(amount.textContent) + newVal;
+  if (updatedAmount <= 0) {
+    updatedAmount = 1;
+  }
+  amount.textContent = updatedAmount;
+};
+
+for (const plusBtn of plusBtns) {
+  plusBtn.addEventListener("click", updateAmount);
+}
+for (const minusBtn of minusBtns) {
+  minusBtn.addEventListener("click", updateAmount);
+}
